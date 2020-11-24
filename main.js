82528979c598bdc6fbcd769cac8706bd47bcb874
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-11-24 15:30:40 +0100

    search done

--- main.js
+++ main.js
@@ -4,12 +4,18 @@ var app =  new Vue ({
         searchInput: '',
         movies: []
     },
-    mounted() {
-        axios.get('https://api.themoviedb.org/3/search/movie', {
-            params: {
-                api_key: 'c3629f71ee7deef7be9c4792c3632882',
-                query: 'superman'
-            }
-        }).then(result => console.log(result));
+    methods: {
+        btnEnter() {
+            console.log(this.searchInput);
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    api_key: 'c3629f71ee7deef7be9c4792c3632882',
+                    query: this.searchInput
+                }
+            }).then(result => {
+                this.movies = result.data.results;
+                console.log(this.movies);
+            });
+        }
     }
 });
