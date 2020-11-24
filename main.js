ae72d98b4ef5063541c98e3af2ba97a827408f49
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-11-24 16:29:46 +0100

    tried the changes flags . but doesnt work

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var app =  new Vue ({
     el: '#root',
     data: {
         searchInput: '',
-        movies: []
+        movies: [],
+        currentLang:''
     },
     methods: {
         btnEnter() {
@@ -14,7 +15,49 @@ var app =  new Vue ({
                 }
             }).then(result => {
                 this.movies = result.data.results;
-                console.log(this.movies);
+                // console.log(this.movies);
+                this.movies.forEach((item, i) => {
+                    this.flag()
+                });
+
+            });
+        },
+        flag() {
+            this.movies.forEach((movie) => {
+                // console.log(movie.original_language);
+                // switch (movie.original_language) {
+                //     case 'it':
+                //         this.currentLang = 'flags/italy.png'
+                //         break;
+                //     case 'en':
+                //         this.currentLang = 'flags/eng.png'
+                //         break;
+                //     case 'fr':
+                //         this.currentLang = 'flags/fra.png'
+                //         break;
+                //     case 'de':
+                //         this.currentLang = 'flags/germ.png'
+                //         break;
+                //     case 'es':
+                //         this.currentLang = 'flags/spain.png'
+                //         break;
+                //     default:
+                //         break;
+                //
+                // }
+                if (movie.original_language == 'it') {
+                    this.currentLang = 'flags/italy.png'
+                } else if (movie.original_language == 'en') {
+                    this.currentLang = 'flags/eng.png'
+                } else if (movie.original_language == 'fr') {
+                    this.currentLang = 'flags/fra.png'
+                } else if (movie.original_language == 'de') {
+                    this.currentLang = 'flags/germ.png'
+                } else if (movie.original_language == 'es') {
+                    this.currentLang =  'flags/spain.png'
+
+                }
+
             });
         }
     }
