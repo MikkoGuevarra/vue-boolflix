8f681cf00b26c015daecdadf0640aee98df2018a
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-11-24 13:03:43 +0100

    tried semi static semi dinamic

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 var app =  new Vue ({
     el: '#root',
     data: {
-
+        searchInput: '',
+        movies: []
     },
-    methods: {
-
+    mounted() {
+        axios.get('https://api.themoviedb.org/3/search/movie', {
+            params: {
+                api_key: 'c3629f71ee7deef7be9c4792c3632882',
+                query: 'superman'
+            }
+        }).then(result => console.log(result));
     }
 });
