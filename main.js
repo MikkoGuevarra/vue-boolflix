2bd5030e11232f3f45103dd849bc3551c80b7df4
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-11-25 17:11:13 +0100

    added static img, now im gonna try dinamic

--- main.js
+++ main.js
@@ -8,39 +8,42 @@ var app =  new Vue ({
     },
     methods: {
         btnEnter() {
+            if (this.searchInput.trim() != '') {
+                axios.get('https://api.themoviedb.org/3/search/movie', {
+                    params: {
+                        api_key: 'c3629f71ee7deef7be9c4792c3632882',
+                        query: this.searchInput
+                    }
+                }).then(result => {
+                    this.movies = result.data.results;
+                    console.log(this.movies);
 
-            axios.get('https://api.themoviedb.org/3/search/movie', {
-                params: {
-                    api_key: 'c3629f71ee7deef7be9c4792c3632882',
-                    query: this.searchInput
-                }
-            }).then(result => {
-                this.movies = result.data.results;
-                console.log(this.movies);
 
 
+                    this.movies.forEach((movie) => {
+                        movie.vote_average = Math.round (movie.vote_average / 2);
+                    });
 
-                this.movies.forEach((movie) => {
-                   movie.vote_average = Math.round (movie.vote_average / 2);
                 });
 
-            });
+                axios.get('https://api.themoviedb.org/3/search/tv', {
+                    params: {
+                        api_key: 'c3629f71ee7deef7be9c4792c3632882',
+                        query: this.searchInput
+                    }
+                }).then(result => {
+                    this.shows = result.data.results;
+                    // console.log(this.movies);
+                    // console.log(this.shows);
 
-            axios.get('https://api.themoviedb.org/3/search/tv', {
-                params: {
-                    api_key: 'c3629f71ee7deef7be9c4792c3632882',
-                    query: this.searchInput
-                }
-            }).then(result => {
-                this.shows = result.data.results;
-                // console.log(this.movies);
-                // console.log(this.shows);
+                    this.shows.forEach((show) => {
+                        show.vote_average = Math.round (show.vote_average / 2);
+                    });
 
-                this.shows.forEach((show) => {
-                   show.vote_average = Math.round (show.vote_average / 2);
                 });
+                this.searchInput= "";
+            }
 
-            });
         }
     }
 });
