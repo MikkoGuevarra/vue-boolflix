74cda399013fe584430f10e28b7a7ec4a6db27dd
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-11-25 15:43:31 +0100

    added tv shows

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ var app =  new Vue ({
     data: {
         searchInput: '',
         movies: [],
+        shows:[],
         totStars: 5
     },
     methods: {
         btnEnter() {
-            console.log(this.searchInput);
+
             axios.get('https://api.themoviedb.org/3/search/movie', {
                 params: {
                     api_key: 'c3629f71ee7deef7be9c4792c3632882',
@@ -15,12 +16,28 @@ var app =  new Vue ({
                 }
             }).then(result => {
                 this.movies = result.data.results;
-                // console.log(this.movies);
+                console.log(this.movies);
 
 
 
                 this.movies.forEach((movie) => {
                    movie.vote_average = Math.round (movie.vote_average / 2);
+                });
+
+            });
+
+            axios.get('https://api.themoviedb.org/3/search/tv', {
+                params: {
+                    api_key: 'c3629f71ee7deef7be9c4792c3632882',
+                    query: this.searchInput
+                }
+            }).then(result => {
+                this.shows = result.data.results;
+                // console.log(this.movies);
+                // console.log(this.shows);
+
+                this.shows.forEach((show) => {
+                   show.vote_average = Math.round (show.vote_average / 2);
                 });
 
             });
